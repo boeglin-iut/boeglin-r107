e652e1e081c64cc663baf0093a26f72f57fa55c6
boeglin-iut <boeglin-iut@gmail.com>
2023-10-26 11:11:57 +0200

    fix

--- TP2-CB/TP2-exo8.py
+++ TP2-CB/TP2-exo8.py
@@ -1,10 +1,10 @@
 x=float(input("Entrez un nombre décimal : "))
 
-if x >= 2 and x < 3:
+if x > 2 or x == 2 and x < 3:
     print("x appartient à I")
-elif x > 0 and x <= 1:
+elif x > 0 and x < 1 or x == 1:
     print("x appartient à I")
-elif x >= -10 and x <= -2:
+elif x > -10 or x == 10 and x < -2 or x == 2:
     print("x appartient à I")
 else:
     print("x n'appartient pas à I")
