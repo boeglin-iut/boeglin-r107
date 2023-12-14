19bc226ef6061621222717ed148fe2e9b601836c
Xurron <46415255+Xurron@users.noreply.github.com>
2023-12-14 14:13:02 +0100

    add file

---
+++ TP5-CB/tp5exo4.py
@@ -0,0 +1,18 @@
+somme = int(input("Entrez la somme : "))
+
+billet100 = somme // 100
+somme = somme % 100
+
+billet50 = somme // 50
+somme = somme % 50
+
+billet10 = somme // 10
+somme = somme % 10
+
+piece2 = somme // 2
+somme = somme % 2
+
+piece1 = somme // 1
+somme = somme % 1
+
+print(f"{somme} euro(s), c'est donc {billet100} billets de 100€, {billet50} billets de 50€, {billet10} billets de 10€, {piece2} pièces de 2€, {piece1} pièce de 1€")
